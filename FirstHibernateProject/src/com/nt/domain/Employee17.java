1548755d0636c73030b4f7d2c55fc157394c70ef
neetish <neetishmaurya222@gmail.com>
2020-07-15 12:06:43 +0530

    first commit

---
+++ FirstHibernateProject/src/com/nt/domain/Employee17.java
@@ -0,0 +1,34 @@
+package com.nt.domain;
+
+public class Employee17 {
+	private int eid;
+	private String firstname;
+	private String lastname;
+	private String email;
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ FirstHibernateProject/src/com/nt/test/SaveObjectTest.java
@@ -0,0 +1,46 @@
+package com.nt.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nt.domain.Employee17;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Configuration cfg=null;
+		SessionFactory factory=null;
+		Session ses=null;
+		Employee17 emp=null;
+		Transaction tx=null;
+		cfg=new Configuration();
+		cfg=cfg.configure("/com/nt/cfgs/hibernate.cfg.xml");
+		factory=cfg.buildSessionFactory();
+		ses=factory.openSession();
+		emp=new Employee17();
+		emp.setEid(1001);
+		emp.setFirstname("Neetish");
+		emp.setLastname("Maurya");
+		emp.setEmail("neetishmaurya111");
+		try {
+			tx=ses.beginTransaction();
+			ses.save(emp);
+			tx.commit();
+			System.out.println("object is saved/record is inserted");
+		}
+		catch(Exception e)
+		{
+			tx.rollback();
+		}
+		ses.close();
+		factory.close();
+		
+		
+		
+
+	}
+
+}
